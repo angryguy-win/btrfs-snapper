264f9503769803cbf28c4214bf19cd4132d54224
ssnow <162399710+angryguy-win@users.noreply.github.com>
2024-06-30 02:21:50 -0400

    Update btrfs-snapper.sh

--- btrfs-snapper.sh
+++ btrfs-snapper.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
-
+#github-action genshdoc
+#
 ## Create an Install setup script point to the scripts..
 
+# Find the name of the folder the scripts are in
+set -a
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/scripts
+CONFIGS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/configs
+set +a
+
+    ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
+      source $CONFIGS_DIR/setup.conf
+    cp -v *.log /mnt/home/$USERNAME

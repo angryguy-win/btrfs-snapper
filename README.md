7a7b6222d0171845179075b93983f935df9747f3
ssnow <162399710+angryguy-win@users.noreply.github.com>
2024-06-30 02:10:14 -0400

    First upload

---
+++ README.md
@@ -0,0 +1,101 @@
+# BTRFS Snapper
+
+This is currently a work in progress 
+
+
+## Boot Arch ISO
+From initial Prompt type the following commands:
+
+```
+pacman -Sy git
+git clone https://github.com/ChrisTitusTech/ArchTitus
+cd ArchTitus
+./archtitus.sh
+```
+
+## System Description
+This is completely automated arch script. 
+It includes prompts to select your desired disk drive to install and setup
+BTRFS, Snapper, btrfs-grub, snap-pac
+
+Snapper is a tool for managing BTRFS snapshots. It can create and restore snapshots, and provides scheduled auto-snapping. Snap-pac provides a Pacman hook that uses Snapper to create pre- and post- BTRFS snapshots triggered by use of the package manager.
+
+Snapshots can be created manualy or whit the Timeline scheduler.
+it will also prune the timeline and install snapshot automaticaly when 
+they reach the posted threshold that you set in the config.
+
+which  can be found:
+
+```
+/etc/snapper/configs/root
+```
+
+You can rollback after bad installs
+
+If you have a catustrophic failure you can load any desired snapshot from the Grub menu.
+*** Note: that all snapshot whit the exception of the current default will be READ ONLY!.
+you will need to enable write priledges to the snapshot of choise.
+
+snapper ls                  - List the snapshots
+snapper delete 20-30        - Will delete snapshots id 20 to 30  *2* Note:
+snapper rollback 20..21     - Will rollback from 20 to 21 reverse the number to chanbe back 21..20
+
+sudo btrfs subvol show /    - Show a list of the of the Subvolumes and other info.
+sudo btrfs subvol list /    - List all the subvolumes and there ID's.
+sudo snapper -c root create -d "**Base system install**"    - Manual snapshots
+
+
+*2* Note: When deleting a pre snapshot, you should always delete its corresponding post snapshot and vice versa.
+
+*** Note:
+
+    When taking a snapshot of @ (mounted at the root /), other subvolumes are not included in the snapshot. Even if a subvolume is nested below @, a snapshot of @ will not include it. 
+    Create snapper configurations for additional subvolumes besides @ of which you want to keep snapshots.
+
+## System rollback the 'Arch Way'
+Snapper includes a rollback tool, but on Arch systems the preferred method is a manual rollback.
+
+After booting into a snapshot mounted rw courtesy of overlayfs, mount the toplevel subvolume (subvolid=5). That is, omit any subvolid or subvol mount flags (example: an encrypted device map labelled cryptdev) ...
+```
+sudo mount /dev/mapper/cryptdev /mnt
+```
+Move the broken @ subvolume out of the way ...
+
+```
+sudo mv /mnt/@ /mnt/@.broken
+```
+Or simply delete the subvolume ...
+```
+sudo btrfs subvolume delete /mnt/@
+```
+Find the number of the snapshot that you want to recover ...
+```
+sudo grep -r '<date>' /mnt/@snapshots/*/info.xml
+[...]
+/.snapshots/8/info.xml:  <date>2022-08-20 15:21:53</date>
+/.snapshots/9/info.xml:  <date>2022-08-20 15:22:39</date>
+```
+Create a read-write snapshot of the read-only snapshot taken by Snapper ...
+```
+sudo btrfs subvolume snapshot /mnt/@snapshots/number/snapshot /mnt/@
+```
+Where number is the snapshot you wish to restore as the new @.
+
+Unmount /mnt.
+
+Reboot and rollback!
+
+
+
+You can proceed to install snapper-gui and btrfs-assitant after the install to help
+manage the snapshot nad your btrfa drive.
+
+
+More information can be found in the arch wiki:
+```
+https://wiki.archlinux.org/title/snapper
+```
+
+![alt text](image-1.png)
+
+![alt text](image.png)
---
+++ scripts/startup.sh
@@ -0,0 +1,237 @@
+#!/usr/bin/env bash
+
+logo () {
+# This will be shown on every set as user is progressing
+echo -ne "
+-------------------------------------------------------------------------
+ █████╗ ██████╗  ██████╗██╗  ██╗████████╗██╗████████╗███████╗
+██╔══██╗██╔══██╗██╔════╝██║  ██║╚══██╔══╝██║╚══██╔══╝██╔════╝
+███████║██████╔╝██║     ███████║   ██║   ██║   ██║   ███████╗
+██╔══██║██╔══██╗██║     ██╔══██║   ██║   ██║   ██║   ╚════██║
+██║  ██║██║  ██║╚██████╗██║  ██║   ██║   ██║   ██║   ███████║
+╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚═╝   ╚═╝   ╚══════╝
+------------------------------------------------------------------------
+            Please select presetup settings for your system              
+------------------------------------------------------------------------
+"
+}
+
+# @setting CONFIG_FILE string[$CONFIGS_DIR/setup.conf] Location of setup.conf to be used by set_option and all subsequent scripts. 
+CONFIG_FILE=$CONFIGS_DIR/setup.conf
+if [ ! -f $CONFIG_FILE ]; then # check if file exists
+    touch -f $CONFIG_FILE # create file if not exists
+fi
+
+# @description set options in setup.conf
+# @arg $1 string Configuration variable.
+# @arg $2 string Configuration value.
+set_option() {
+    if grep -Eq "^${1}.*" $CONFIG_FILE; then # check if option exists
+        sed -i -e "/^${1}.*/d" $CONFIG_FILE # delete option if exists
+    fi
+    echo "${1}=${2}" >>$CONFIG_FILE # add option
+}
+
+# @description This function will handle file systems. At this movement we are handling only
+# btrfs and ext4. Others will be added in future.
+filesystem () {
+echo -ne "
+Please Select your file system for both boot and root
+"
+options=("btrfs" "ext4" "luks" "exit")
+select_option $? 1 "${options[@]}"
+
+case $? in
+0) set_option FS btrfs;;
+1) set_option FS ext4;;
+2) 
+    set_password "LUKS_PASSWORD"
+    set_option FS luks
+    ;;
+3) exit ;;
+*) echo "Wrong option please select again"; filesystem;;
+esac
+}
+
+# @description Choose whether drive is SSD or not.
+drivessd () {
+echo -ne "
+Is this an ssd? yes/no:
+"
+
+options=("Yes" "No")
+select_option $? 1 "${options[@]}"
+
+case ${options[$?]} in
+    y|Y|yes|Yes|YES)
+    set_option MOUNT_OPTIONS "noatime,compress=zstd,ssd,commit=120";;
+    n|N|no|NO|No)
+    set_option MOUNT_OPTIONS "noatime,compress=zstd,commit=120";;
+    *) echo "Wrong option. Try again";drivessd;;
+esac
+}
+
+# @description Disk selection for drive to be used with installation.
+diskpart () {
+echo -ne "
+------------------------------------------------------------------------
+    THIS WILL FORMAT AND DELETE ALL DATA ON THE DISK
+    Please make sure you know what you are doing because
+    after formating your disk there is no way to get data back
+------------------------------------------------------------------------
+
+"
+
+PS3='
+Select the disk to install on: '
+options=($(lsblk -n --output TYPE,KNAME,SIZE | awk '$1=="disk"{print "/dev/"$2"|"$3}'))
+
+select_option $? 1 "${options[@]}"
+disk=${options[$?]%|*}
+
+echo -e "\n${disk%|*} selected \n"
+    set_option DISK ${disk%|*}
+
+drivessd
+}
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Installing Prerequisites
+-------------------------------------------------------------------------
+"
+pacman -S --noconfirm --needed gptfdisk btrfs-progs glibc btrfs-grub snap-pac snapper
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Formating Disk
+-------------------------------------------------------------------------
+"
+umount -A --recursive /mnt # make sure everything is unmounted before we start
+# disk prep
+sgdisk -Z ${DISK} # zap all on disk
+sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+
+# create partitions
+sgdisk -n 1::+1M --typecode=1:ef02 --change-name=1:'BIOSBOOT' ${DISK} # partition 1 (BIOS Boot Partition)
+sgdisk -n 2::+300M --typecode=2:ef00 --change-name=2:'EFIBOOT' ${DISK} # partition 2 (UEFI Boot Partition)
+sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'ROOT' ${DISK} # partition 3 (Root), default start, remaining
+if [[ ! -d "/sys/firmware/efi" ]]; then # Checking for bios system
+    sgdisk -A 1:set:2 ${DISK}
+fi
+partprobe ${DISK} # reread partition table to ensure it is correct
+
+# make filesystems
+echo -ne "
+-------------------------------------------------------------------------
+                    Creating Filesystems
+-------------------------------------------------------------------------
+"
+# @description Creates the btrfs subvolumes. 
+createsubvolumes () {
+    btrfs subvolume create /mnt/@
+    btrfs subvolume create /mnt/@home
+    btrfs subvolume create /mnt/@var
+    btrfs subvolume create /mnt/@tmp
+    btrfs subvolume create /mnt/@.snapshots
+}
+
+# @description Mount all btrfs subvolumes after root has been mounted.
+mountallsubvol () {
+    mount -o ${MOUNT_OPTIONS},subvol=@home ${partition3} /mnt/home
+    mount -o ${MOUNT_OPTIONS},subvol=@tmp ${partition3} /mnt/tmp
+    mount -o ${MOUNT_OPTIONS},subvol=@var ${partition3} /mnt/var
+    mount -o ${MOUNT_OPTIONS},subvol=@.snapshots ${partition3} /mnt/.snapshots
+}
+
+# @description BTRFS subvolulme creation and mounting. 
+subvolumesetup () {
+# create nonroot subvolumes
+    createsubvolumes     
+# unmount root to remount with subvolume 
+    umount /mnt
+# mount @ subvolume
+    mount -o ${MOUNT_OPTIONS},subvol=@ ${partition3} /mnt
+# make directories home, .snapshots, var, tmp
+    mkdir -p /mnt/{home,var,tmp,.snapshots}
+# mount subvolumes
+    mountallsubvol
+}
+
+if [[ "${DISK}" =~ "nvme" ]]; then
+    partition2=${DISK}p2
+    partition3=${DISK}p3
+else
+    partition2=${DISK}2
+    partition3=${DISK}3
+fi
+
+if [[ "${FS}" == "btrfs" ]]; then
+    mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
+    mkfs.btrfs -L ROOT ${partition3} -f
+    mount -t btrfs ${partition3} /mnt
+    subvolumesetup
+elif [[ "${FS}" == "ext4" ]]; then
+    mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
+    mkfs.ext4 -L ROOT ${partition3}
+    mount -t ext4 ${partition3} /mnt
+elif [[ "${FS}" == "luks" ]]; then
+    mkfs.vfat -F32 -n "EFIBOOT" ${partition2}
+# enter luks password to cryptsetup and format root partition
+    echo -n "${LUKS_PASSWORD}" | cryptsetup -y -v luksFormat ${partition3} -
+# open luks container and ROOT will be place holder 
+    echo -n "${LUKS_PASSWORD}" | cryptsetup open ${partition3} ROOT -
+# now format that container
+    mkfs.btrfs -L ROOT ${partition3}
+# create subvolumes for btrfs
+    mount -t btrfs ${partition3} /mnt
+    subvolumesetup
+# store uuid of encrypted partition for grub
+    echo ENCRYPTED_PARTITION_UUID=$(blkid -s UUID -o value ${partition3}) >> $CONFIGS_DIR/setup.conf
+fi
+
+# mount target
+mkdir -p /mnt/boot/efi
+mount -t vfat -L EFIBOOT /mnt/boot/
+
+if ! grep -qs '/mnt' /proc/mounts; then
+    echo "Drive is not mounted can not continue"
+    echo "Rebooting in 3 Seconds ..." && sleep 1
+    echo "Rebooting in 2 Seconds ..." && sleep 1
+    echo "Rebooting in 1 Second ..." && sleep 1
+    reboot now
+fi
+
+echo -ne "
+-------------------------------------------------------------------------
+                    Enabling Essential Services
+-------------------------------------------------------------------------
+"
+
+
+if [[ "${FS}" == "luks" || "${FS}" == "btrfs" ]]; then
+echo -ne "
+-------------------------------------------------------------------------
+                    Creating Snapper Config
+-------------------------------------------------------------------------
+"
+
+SNAPPER_CONF="$HOME/ArchTitus/configs/etc/snapper/configs/root"
+mkdir -p /etc/snapper/configs/
+cp -rfv ${SNAPPER_CONF} /etc/snapper/configs/
+
+SNAPPER_CONF_D="$HOME/ArchTitus/configs/etc/conf.d/snapper"
+mkdir -p /etc/conf.d/
+cp -rfv ${SNAPPER_CONF_D} /etc/conf.d/
+
+fi
+
+
+# Starting functions
+logo
+diskpart
+clear
+logo
+filesystem
+clear
+

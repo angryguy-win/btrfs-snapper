cc5616d1d21d52b8c5f778e8e4fe4fe9e898675b
ssnow <162399710+angryguy-win@users.noreply.github.com>
2024-07-05 16:56:01 -0400

    Update btrfs-snapper.sh

--- btrfs-snapper.sh
+++ btrfs-snapper.sh
@@ -15,6 +15,6 @@ SCRIPTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd 
 CONFIGS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/configs
 set +a
 
-    ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
-      source $CONFIGS_DIR/setup.conf
-    cp -v *.log /mnt/home/$USERNAME
+( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
+source $CONFIGS_DIR/setup.conf
+cp -v *.log /mnt/home/$USERNAME
